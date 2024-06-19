0258578834fb2ec3160e742e071b2eef0f38b7fa
Thomas Rothenbächer <thomas.rothenbaecher@t-systems.com>
2024-06-19 11:32:14 +0200

    Completed Pipeline

---
+++ src/basic_cleaning/run.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+"""
+Performs basic cleaning on the data and save the results in Weights & Biases
+"""
+import argparse
+import logging
+import wandb
+import pandas as pd
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def go(args):
+
+    run = wandb.init(job_type="basic_cleaning")
+    run.config.update(args)
+
+    # Download input artifact. This will also log that this script is using this
+    # particular version of the artifact
+    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+
+    local_path = wandb.use_artifact("sample.csv:latest").file()
+    df = pd.read_csv(local_path)
+    min_price = args.min_price
+    max_price = args.max_price
+    idx = df["price"].between(min_price, max_price)
+    df = df[idx].copy()
+    df["last_review"] = pd.to_datetime(df["last_review"])
+    df.to_csv("clean_sample.csv", index=False)
+    logger.info("Cleaned df saved.")
+
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    artifact.add_file("clean_sample.csv")
+    run.log_artifact(artifact)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="This step cleans the data.")
+
+    parser.add_argument(
+        "--input_artifact",
+        type=str,
+        help="Csv as input to dataframe.",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--output_artifact",
+        type=str,
+        help="Name of output csv.",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--output_type",
+        type=str,
+        help="Artifact type in W & B.",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--output_description",
+        type=str,
+        help="Description of output artifact.",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--min_price",
+        type=float,
+        help="Remove all entries with 'price' below min_price from the dataframe.",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--max_price",
+        type=float,
+        help="Remove all entries with 'price' higher than max_price from the dataframe.",
+        required=True,
+    )
+
+    args = parser.parse_args()
+
+    go(args)
