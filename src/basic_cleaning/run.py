74f66913864c452a9aa451915e5a5d9733107c73
Thomas Rothenbächer <thomas.rothenbaecher@t-systems.com>
2024-06-19 12:07:48 +0200

    Geolocation filter

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -26,6 +26,8 @@ def go(args):
     max_price = args.max_price
     idx = df["price"].between(min_price, max_price)
     df = df[idx].copy()
+    idx = df["longitude"].between(-74.25, -73.50) & df["latitude"].between(40.5, 41.2)
+    df = df[idx].copy()
     df["last_review"] = pd.to_datetime(df["last_review"])
     df.to_csv("clean_sample.csv", index=False)
     logger.info("Cleaned df saved.")
